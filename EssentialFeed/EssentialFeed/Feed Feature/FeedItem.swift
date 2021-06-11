1e517407a225be29e8869892ee57b3da850a3071
Luis Zapata <lalejzapata@falabella.cl>
2021-06-10 22:21:53 -0400

    Base Project

---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright © 2018 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public struct FeedItem: Equatable {
+	public let id: UUID
+	public let description: String?
+	public let location: String?
+	public let imageURL: URL
+	
+	public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+		self.id = id
+		self.description = description
+		self.location = location
+		self.imageURL = imageURL
+	}
+}
---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2018 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public enum LoadFeedResult {
+	case success([FeedItem])
+	case failure(Error)
+}
+
+public protocol FeedLoader {
+	func load(completion: @escaping (LoadFeedResult) -> Void)
+}
